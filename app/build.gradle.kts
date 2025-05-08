6c462463c02c55479f1580891cb6e531eb6d931f
Tej <nikamtejaswini64@gmail.com>
2025-05-08 11:06:32 +0530

    Changed AddActivity Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
